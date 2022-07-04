c78a1675edb539022758f106e09b641731b8e988
master <minhhhuyle123@gmail.com>
2022-07-04 20:22:10 +0700

    first commit

---
+++ Data/create_dataset_1.py
@@ -0,0 +1,150 @@
+"""
+This script to create .csv videos frames action annotation file.
+
+- It will play a video frame by frame control the flow by [a] and [d]
+ to play previos or next frame.
+- Open the annot_file (.csv) and label each frame of video with number
+ of action class.
+"""
+#unset SESSION_MANAGER
+import os
+import cv2
+import time
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class_names = ['Standing', 'Walking', 'Sitting', 'Lying Down','Stand up', 'Sit down', 'Fall Down']
+
+video_folder = '/home/minhhuy/Desktop/Python/Human-Falling-Detect-Tracks/Data/falldata/Home/Videos'
+annot_file = '../Data/Home_new.csv'
+annot_file_2 = '../Data/Home_new_2.csv'
+
+
+def create_csv(folder):
+    list_file = sorted(os.listdir(folder))
+    cols = ['video', 'frame', 'label']
+    df = pd.DataFrame(columns=cols)
+    for fil in list_file:
+        cap = cv2.VideoCapture(os.path.join(folder, fil))
+        frames_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        video = np.array([fil] * frames_count)
+        frame = np.arange(1, frames_count + 1)
+        label = np.array([0] * frames_count)
+        rows = np.stack([video, frame, label], axis=1)
+        df = df.append(pd.DataFrame(rows, columns=cols),
+                       ignore_index=True)
+        cap.release()
+    df.to_csv(annot_file, index=False)
+
+
+if not os.path.exists(annot_file):
+    create_csv(video_folder)
+
+annot = pd.read_csv(annot_file)
+video_list = annot.iloc[:, 0].unique()
+
+cols = ['video', 'frame', 'label']
+df = pd.DataFrame(columns=cols)
+
+index_video_to_play = 0
+backVideo = False
+step = 0
+while index_video_to_play < len(video_list):
+    video_file = os.path.join(video_folder, video_list[index_video_to_play])
+    print(os.path.basename(video_file))
+
+    annot_2 = annot[annot['video'] == video_list[index_video_to_play]].reset_index(drop=True)
+    frames_idx = annot_2.iloc[:, 1].tolist()
+
+    cap = cv2.VideoCapture(video_file)
+    frames_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    assert frames_count == len(frames_idx), 'frame count not equal! {} and {}'.format(
+        len(frames_idx), frames_count)
+
+    video = np.array([video_list[index_video_to_play]] * frames_count)
+    frame_1 = np.arange(1, frames_count + 1)
+    label = np.array([0] * frames_count)
+    if backVideo is True and step == 1:
+        step = 2
+    k = 0
+    i = 0
+    ipre = - 1
+    while True:
+        cap.set(cv2.CAP_PROP_POS_FRAMES, i)
+        ret, frame = cap.read()
+        if i > ipre and i > 0:
+            label[i - 1] = k
+            ipre = i
+        if ret:
+            cls_name = class_names[label[i - 1]]
+            frame = cv2.resize(frame, (0, 0), fx=3.5, fy=3.5)
+            frame = cv2.putText(frame, 'Videos: {}     Total_frames: {}        Frame: {}       Pose: {} '.format(
+                video_list[index_video_to_play], frames_count, i + 1, cls_name),
+                                (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 0, 0), 2)
+
+            frame = cv2.putText(frame, 'Back:  a', (10, 270), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+            frame = cv2.putText(frame, 'Standing:   0', (10, 300), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+            frame = cv2.putText(frame, 'Walking:    1', (10, 330), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+            frame = cv2.putText(frame, 'Sitting:    2', (10, 360), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+            frame = cv2.putText(frame, 'Lying Down: 3', (10, 390), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+            frame = cv2.putText(frame, 'Stand Up:   4', (10, 420), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+            frame = cv2.putText(frame, 'Sit Down:   5', (10, 450), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+            frame = cv2.putText(frame, 'Fall Down:  6', (10, 480), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+            cv2.imshow('frame', frame)
+
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q'):
+                break
+            elif key == ord('0'):  # standing
+                i += 1
+                k = 0
+                continue
+            elif key == ord('1'):  # walking
+                i += 1
+                k = 1
+                continue
+            elif key == ord('2'):  # sitting
+                i += 1
+                k = 2
+            elif key == ord('3'):  # lying down
+                i += 1
+                k = 3
+                continue
+            elif key == ord('4'):  # stand up
+                i += 1
+                k = 4
+            elif key == ord('5'):  # sit down
+                i += 1
+                k = 5
+                continue
+            elif key == ord('6'):  # fall down
+                i += 1
+                k = 6
+                continue
+            elif key == ord('a'):  # back
+                i -= 1
+                continue
+            elif key == ord('b'):  # back video
+                backVideo = True
+                step = 1
+                index_video_to_play -= 1
+                break
+        else:
+            break
+    if backVideo == False:
+        rows = np.stack([video, frame_1, label], axis=1)
+        df = df.append(pd.DataFrame(rows, columns=cols), ignore_index=True)
+        index_video_to_play += 1
+    elif backVideo is True and step == 2:
+        rows = np.stack([video, frame_1, label], axis=1)
+        df.iloc[len(df)-frames_count:len(df), :] = rows
+        index_video_to_play += 1
+        backVideo = False
+        step = 0
+
+df.to_csv(annot_file_2, index=False)
+cap.release()
+cv2.destroyAllWindows()
+
---
+++ Data/create_dataset_3.py
@@ -0,0 +1,152 @@
+
+"""
+This script to create dataset and labels by clean off some NaN, do a normalization,
+label smoothing and label weights by scores.
+"""
+import pickle
+import numpy as np
+import pandas as pd
+from collections import Counter
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+class_names = ['Standing', 'Walking', 'Sitting', 'Lying Down', 'Stand up', 'Sit down', 'Fall Down']
+main_parts = ['LShoulder_x', 'LShoulder_y', 'RShoulder_x', 'RShoulder_y', 'LHip_x', 'LHip_y',
+              'RHip_x', 'RHip_y']
+main_idx_parts = [1, 2, 7, 8, -1]  # 1.5
+csv_pose_file = '../Data/AllStep2.csv'
+save_path = '../Data/train.pkl'
+
+# Params.
+smooth_labels_step = 8
+n_frames = 30
+skip_frame = 1
+
+annot = pd.read_csv(csv_pose_file)
+
+# Remove NaN.
+idx = annot.iloc[:, 2:-1][main_parts].isna().sum(1) > 0
+idx = np.where(idx)[0]
+annot = annot.drop(idx)
+# One-Hot Labels.
+label_onehot = pd.get_dummies(annot['label'])
+annot = annot.drop('label', axis=1).join(label_onehot)  # annot = [video, frame, 13, label(1 0 0 0 0 0 0)]  ...x48
+cols = label_onehot.columns.values  # cols = [0, 1, 2, 3, 4, 5, 6]
+
+
+def scale_pose(xy):
+    """
+    Normalize pose points by scale with max/min value of each pose.
+    xy : (frames, parts, xy) or (parts, xy)
+    """
+    if xy.ndim == 2:
+        xy = np.expand_dims(xy, 0)
+    xy_min = np.nanmin(xy, axis=1)
+    xy_max = np.nanmax(xy, axis=1)
+    for i in range(xy.shape[0]):
+        xy[i] = ((xy[i] - xy_min[i]) / (xy_max[i] - xy_min[i])) * 2 - 1
+    return xy.squeeze()
+
+
+def seq_label_smoothing(labels, max_step=10):
+    steps = 0
+    remain_step = 0
+    target_label = 0
+    active_label = 0
+    start_change = 0
+    max_val = np.max(labels)
+    min_val = np.min(labels)
+    for i in range(labels.shape[0]):
+        if remain_step > 0:
+            if i >= start_change:
+                labels[i][active_label] = max_val * remain_step / steps
+                labels[i][target_label] = max_val * (steps - remain_step) / steps \
+                    if max_val * (steps - remain_step) / steps else min_val
+                remain_step -= 1
+            continue
+
+        diff_index = np.where(np.argmax(labels[i:i+max_step], axis=1) - np.argmax(labels[i]) != 0)[0]
+        if len(diff_index) > 0:
+            start_change = i + remain_step // 2
+            steps = diff_index[0]
+            remain_step = steps
+            target_label = np.argmax(labels[i + remain_step])
+            active_label = np.argmax(labels[i])
+    return labels
+
+
+def graphSample(labels):
+    class_num = np.zeros(7)
+    labels = labels.argmax(axis=1)
+    s = Counter(labels)
+    for i in s:
+        class_num[i] = s[i]
+    idx_class = range(7)
+    plt.bar(idx_class, class_num)
+    plt.xticks(idx_class, class_names)
+
+    for x, y in zip(idx_class, class_num):
+        plt.text(x + 0.02, y + 0.05, '%d' % y, ha='center', va='bottom')
+    plt.savefig('../Data/graph.jpg')
+    plt.show()
+
+
+feature_set = np.empty((0, n_frames, 14, 3))
+labels_set = np.empty((0, len(cols)))
+vid_list = annot['video'].unique()
+for vid in vid_list:
+    print(f'Process on: {vid}')
+    data = annot[annot['video'] == vid].reset_index(drop=True).drop(columns='video')  # data = [frame, 13, label(binary)] ...x47
+
+    # Label Smoothing.
+    esp = 0.1
+    data[cols] = data[cols] * (1 - esp) + (1 - data[cols]) * esp / (len(cols) - 1)
+    data[cols] = seq_label_smoothing(data[cols].values, smooth_labels_step)
+
+    # Separate continuous frames.
+    frames = data['frame'].values
+    frames_set = []
+    fs = [0]
+    for i in range(1, len(frames)):
+        if frames[i] < frames[i-1] + 10:
+            fs.append(i)
+        else:
+            frames_set.append(fs)
+            fs = [i]
+    frames_set.append(fs)
+
+    for fs in frames_set:
+        xys = data.iloc[fs, 1:-len(cols)]  # xys = [13]
+        xys = xys.values.reshape(-1, 13, 3)  # xys = [[[1], [2], [3],....,[13]],.....]
+        # Scale pose normalize.
+        xys[:, :, :2] = scale_pose(xys[:, :, :2])  # x and y
+        # Add center point.
+        xys = np.concatenate((xys, np.expand_dims((xys[:, 1, :] + xys[:, 2, :]) / 2, 1)), axis=1)
+        # xys = [[[1], [2], [3],....,[13], [center point]],.....]
+
+        # Weighting main parts score.
+        scr = xys[:, :, -1].copy()
+        scr[:, main_idx_parts] = np.minimum(scr[:, main_idx_parts] * 1.5, 1.0)  # [1, 2, 7, 8, -1]
+        # ['LShoulder_s', 'RShoulder_s', 'LHip_s', 'RHip_s', 'center point']
+
+        # Mean score.
+        scr = scr.mean(1)
+
+        # Targets.
+        lb = data.iloc[fs, -len(cols):].values
+        # Apply points score mean to all labels.
+        lb = lb * scr[:, None]
+
+        for i in range(xys.shape[0] - n_frames):  # xys > n_frames = 30
+            feature_set = np.append(feature_set, xys[i:i+n_frames][None, ...], axis=0)
+            labels_set = np.append(labels_set, lb[i:i+n_frames].mean(0)[None, ...], axis=0)
+
+    # with open(save_path, 'wb') as f:
+    #     pickle.dump((feature_set, labels_set), f)
+xtrain, xtest,ytrain, ytest = train_test_split(feature_set, labels_set, train_size=0.7)
+with open('../Data/train07.pkl', 'wb') as f:
+    pickle.dump((xtrain, ytrain), f)
+with open('../Data/test03.pkl', 'wb') as f:
+    pickle.dump((xtest, ytest), f)
+graphSample(labels_set)
+
